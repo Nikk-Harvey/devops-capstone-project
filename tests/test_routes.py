b05ca00719cf1cf64179dc87e0cc497a6045874d
Nikk-Harvey <132252641+Nikk-Harvey@users.noreply.github.com>
2023-05-24 14:43:04 -0500

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -162,18 +162,18 @@ class TestAccountService(TestCase):
         self.assertEqual(updated_account["name"], "Something Known")
         
         """ Should delete account """
-            def test_delete_account(self):
+    def test_delete_account(self):
         """It should Delete an Account"""
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
         
-        def test_method_not_allowed(self):
+    def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
        
-            def test_get_account_list(self):
+    def test_get_account_list(self):
         """It should Get a list of Accounts"""
         self._create_accounts(5)
         resp = self.client.get(BASE_URL)
@@ -181,31 +181,31 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(len(data), 5)
 
-            def test_method_not_allowed(self):
+    def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
         
-            def test_security_headers(self):
-        """It should return security headers"""
-        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        headers = {
-            'X-Frame-Options': 'SAMEORIGIN',
-            'X-XSS-Protection': '1; mode=block',
-            'X-Content-Type-Options': 'nosniff',
-            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
-            'Referrer-Policy': 'strict-origin-when-cross-origin'
-        }
-        for key, value in headers.items():
-            self.assertEqual(response.headers.get(key), value)
+    def test_security_headers(self):
+      """It should return security headers"""
+      response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+      self.assertEqual(response.status_code, status.HTTP_200_OK)
+      headers = {
+        'X-Frame-Options': 'SAMEORIGIN',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Content-Type-Options': 'nosniff',
+        'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+        'Referrer-Policy': 'strict-origin-when-cross-origin'
+      }
+      for key, value in headers.items():
+        self.assertEqual(response.headers.get(key), value)
             
-                def test_cors_security(self):
-        """It should return a CORS header"""
-        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # Check for the CORS header
-        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+    def test_cors_security(self):
+      """It should return a CORS header"""
+      response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+      self.assertEqual(response.status_code, status.HTTP_200_OK)
+      # Check for the CORS header
+      self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
             
             
   
